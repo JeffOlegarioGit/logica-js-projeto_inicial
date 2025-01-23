75aa685a24f681d246f5a4f2d2868fb3152d1bc5
jeffolegario <49287634+jeffolegario@users.noreply.github.com>
2025-01-23 14:51:54 -0300

    versao publicada final

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,47 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let coresAmigos = [];
+
+function adicionarAmigo() {
+    const inputNome = document.getElementById('amigo');
+    const nomeAmigo = inputNome.value.trim();
+    if (nomeAmigo === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    const novaCor = getRandomColor();
+    amigos.push(nomeAmigo);
+    coresAmigos.push(novaCor);
+
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        const novoLi = document.createElement('li');
+        novoLi.textContent = amigos[i];
+        novoLi.style.backgroundColor = coresAmigos[i];
+        listaAmigos.appendChild(novoLi);
+    }
+    inputNome.value = "";
+    inputNome.focus();
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+      alert("Adicione amigos antes de sortear.");
+      return;
+    }
+  
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+    const corSorteada = coresAmigos[indiceSorteado];
+  
+    const resultadoElement = document.getElementById('resultado');
+    resultadoElement.innerHTML = `<li style="background-color: ${corSorteada};">O amigo sorteado foi: ${amigoSorteado}</li>`;
+  }
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
